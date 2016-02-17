62b05c38d4bf4ead78efc66c90de34758c4697a8
Amritpal Singh <amritbox@gmail.com>
2016-02-16 19:19:55 -0500

    added description joinfilterpartition

--- 03_JoinFilter/src/JoinFilterPartitioner.java
+++ 03_JoinFilter/src/JoinFilterPartitioner.java
@@ -3,6 +3,15 @@ import org.apache.hadoop.mapreduce.Partitioner;
 
 public class JoinFilterPartitioner extends Partitioner<Text, Text>{
 
+	/*
+	 * (non-Javadoc)
+	 * We are using multipart key, therefore we needed to impliment a customer partition,
+	 * our multipart key contains join key plus sort flag, we need to partition only by id
+	 * so that both records with the same join key will end up in the same reducer regardless
+	 * of whether the original arrived from foo or bar.
+	 * This is where the magic happens
+	 */
+	
   @Override
   public int getPartition(Text key, Text value, int numberOfReducers) {
     String keyStr = key.toString();
